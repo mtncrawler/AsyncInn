22dd51dfab166f291f5c2c651d86c06a89ea3339
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-10-31 11:48:04 -0700

    added empty services and interfaces for room, hotel, amenities

---
+++ AsyncInn/AsyncInn/Models/Services/AmenitiesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncInn.Models.Interfaces;
+
+namespace AsyncInn.Models.Services
+{
+    public class AmenitiesService : IAmenities
+    {
+
+    }
+}
