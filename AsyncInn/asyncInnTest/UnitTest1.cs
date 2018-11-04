daace7f9a5224a3d5752d2d27a284547dba43ae6
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-04 14:12:21 -0800

    passing all getters and setters tests

--- AsyncInn/asyncInnTest/UnitTest1.cs
+++ AsyncInn/asyncInnTest/UnitTest1.cs
@@ -43,5 +43,137 @@ namespace asyncInnTest
             hotel.Name = "Hotel California";
             Assert.Equal("Hotel California", hotel.Name);
         }
+
+        [Fact]
+        public void CanGetHotelAddress()
+        {
+            Hotel hotel = new Hotel();
+            hotel.Street = "500 Main Street";
+            hotel.City = "Seattle";
+            hotel.State = "WA";
+            hotel.ZipCode = "98109";
+
+            Assert.Equal("500 Main Street", hotel.Street);
+            Assert.Equal("Seattle", hotel.City);
+            Assert.Equal("WA", hotel.State);
+            Assert.Equal("98109", hotel.ZipCode);
+        }
+
+        [Fact]
+        public void CanSetHotelAddress()
+        {
+            Hotel hotel = new Hotel();
+            hotel.Street = "500 Main Street";
+            hotel.City = "Seattle";
+            hotel.State = "WA";
+            hotel.ZipCode = "98109";
+
+            hotel.Street = "123 Front St";
+            hotel.City = "Ballard";
+            hotel.State = "WA";
+            hotel.ZipCode = "98100";
+
+            Assert.Equal("123 Front St", hotel.Street);
+            Assert.Equal("Ballard", hotel.City);
+            Assert.Equal("WA", hotel.State);
+            Assert.Equal("98100", hotel.ZipCode);
+        }
+
+        [Fact]
+        public void CanGetHotelPhone()
+        {
+            Hotel hotel = new Hotel();
+            hotel.Phone = "404-317-5131";
+
+            Assert.Equal("404-317-5131", hotel.Phone);
+        }
+
+        [Fact]
+        public void CanSetHotelPhone()
+        {
+            Hotel hotel = new Hotel();
+            hotel.Phone = "404-317-5131";
+
+            hotel.Phone = "770-410-0233";
+            Assert.Equal("770-410-0233", hotel.Phone);
+        }
+
+        [Fact]
+        public void CanGetHotelRoomNumber()
+        {
+            HotelRoom hr = new HotelRoom();
+            hr.RoomNumber = 666;
+
+            Assert.Equal(666, hr.RoomNumber);
+        }
+
+        [Fact]
+        public void CanSetHotelRoomNumber()
+        {
+            HotelRoom hr = new HotelRoom();
+            hr.RoomNumber = 666;
+
+            hr.RoomNumber = 999;
+            Assert.Equal(999, hr.RoomNumber);
+        }
+
+        [Fact]
+        public void CanGetHotelRoomRate()
+        {
+            HotelRoom hr = new HotelRoom();
+            hr.Rate = 199;
+
+            Assert.Equal(199, hr.Rate);
+        }
+
+        [Fact]
+        public void CanSetHotelRoomRate()
+        {
+            HotelRoom hr = new HotelRoom();
+            hr.Rate = 199;
+
+            hr.Rate = 100;
+            Assert.Equal(100, hr.Rate);
+        }
+
+        [Fact]
+        public void CanGetAmenity()
+        {
+            Amenities am = new Amenities();
+            am.Name = "A/C";
+
+            Assert.Equal("A/C", am.Name);
+        }
+
+        [Fact]
+        public void CanSetAmenity()
+        {
+            Amenities am = new Amenities();
+            am.Name = "A/C";
+
+            am.Name = "Hottub";
+            Assert.Equal("Hottub", am.Name);
+        }
+
+        [Fact]
+        public void CanGetHotelRoomPetFriendliness()
+        {
+            HotelRoom hr = new HotelRoom();
+            hr.PetFriendly = true;
+
+            Assert.True(hr.PetFriendly);
+        }
+
+        [Fact]
+        public void CanSetHotelRoomPetFriendliness()
+        {
+            HotelRoom hr = new HotelRoom();
+            hr.PetFriendly = true;
+
+            hr.PetFriendly = false;
+            Assert.False(hr.PetFriendly);
+        }
+
+
     }
 }
